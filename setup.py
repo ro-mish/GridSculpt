948f6daf2dca21713fe50f1971de7a5406fd91e7
Rohit Mishra <rohitnmishra2@gmail.com>
2023-04-04 20:39:28 -0700

    setup updated

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     classifiers=[
-        "Intended Audience :: Data Science",
+        "Intended Audience :: Science/Research",
         "Development Status :: 3 - Alpha",
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 3",
